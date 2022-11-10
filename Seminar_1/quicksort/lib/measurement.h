dcf8b16012bcd7a737b3880ccbfac591acb5e31c
adisve <adis.veletanlic@gmail.com>
2022-11-10 20:56:29 +0100

    Finish quik sort and segfault bug

--- Seminar_1/quicksort/lib/measurement.h
+++ Seminar_1/quicksort/lib/measurement.h
@@ -1,6 +1,6 @@
 #ifndef MEASUREMENT_HEADER
 #define MEASUREMENT_HEADER
 
-void measure_recursive_quicksort(int *arr, int *reference_arr, int collection_size, int type);
+void measurequicksort(int *arr, int *reference_arr, int collection_size, int type);
 
 #endif
--- Seminar_1/quicksort/src/main.c
+++ Seminar_1/quicksort/src/main.c
@@ -46,10 +46,8 @@ int main(void)
     for (int i = 0; i < collection_size; i++) reference_arr[i] = arr[i];
 
     /* Measurements for quick sort using RECURSIVE quick sort algorithm */
-    measure_recursive_quicksort(arr, reference_arr, collection_size, RECURSIVE);
-
-    measure_recursive_quicksort(arr, reference_arr, collection_size, ITERATIVE);
+    measurequicksort(arr, reference_arr, collection_size, RECURSIVE);
     /* Measurements for quick sort using recursive quick sort algorithm */
-    // stub
+    measurequicksort(arr, reference_arr, collection_size, ITERATIVE);
     return 0;
 }
