8efc520f62b830083fee3e57d264193af40af32d
Adis Veletanlic <adis.veletanlic@gmail.com>
2022-11-14 21:41:13 +0100

    Fix XOR medianthree function

--- Seminar_1/quicksort/lib/math.c
+++ Seminar_1/quicksort/lib/math.c
@@ -4,11 +4,10 @@
  * performing XOR operation comparing values */
 int medianthree(int *arr, int a, int b, int c)
 {
-	
-	if ((arr[a] <= arr[b]) && (arr[b] <= arr[c])) return b;
-    if ((arr[a] <= arr[c]) && (arr[c] <= arr[b])) return c;
-    if ((arr[b] <= arr[a]) && (arr[a] <= arr[c])) return a;
-    if ((arr[b] <= arr[c]) && (arr[c] <= arr[a])) return c;
-    if ((arr[c] <= arr[a]) && (arr[a] <= arr[b])) return a;
-    return b;
+	if ((arr[a] > arr[b]) ^ (arr[a] > arr[c])) 
+		return a;
+	else if ((arr[b] < arr[a]) ^ (arr[b] < arr[c])) 
+		return b;
+	else
+		return c;
 }
