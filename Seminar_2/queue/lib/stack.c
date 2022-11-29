1d831d3352de9f1b66e435614f6e35ff7b28231b
Adis Veletanlic <adis.veletanlic@gmail.com>
2022-11-29 20:24:05 +0100

    Add seminar 2 task 2

---
+++ Seminar_2/queue/lib/stack.c
@@ -0,0 +1,35 @@
+#include "../lib/stack.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void push(struct stacknode** stack_top, int val)
+{
+        /* Create new node to push to stack */
+        struct stacknode* node = (struct stacknode*)malloc(sizeof(struct stacknode));
+
+        /* Check for stack overflow error */
+        if (node == NULL) stackoverflow_error();
+        
+        node->val = val;
+        node->nextnode = *stack_top;
+        *stack_top = node;
+}
+
+int pop(struct stacknode** stack_top)
+{
+        char x;
+        struct stacknode* top;
+        if (*stack_top == NULL) stackoverflow_error();
+
+        top = *stack_top;
+        x = top->val;
+        *stack_top = top->nextnode;
+        free(top);
+        return x;
+}
+
+void stackoverflow_error()
+{
+        printf("\nStack overflow error\n");
+        exit(0);
+}
